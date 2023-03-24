0bb4e51b8598df82e51750fdbc1c8c309873f7de
[guilhermeas01] <gunichi297@gmail.com>
2023-03-24 11:57:19 -0300

    up

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
 module.exports = {
-  ...nextConfig,
-  ignoreBuildErrors: true,
+  webpack5: true,
+  trailingSlash: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
